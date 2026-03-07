a49f9f0f0aa62d122cc1180947a58e2beaa290e8
bradhack321 <ju0605208@gmail.com>
2026-03-07 22:27:01 +0100

    Update bug.js

--- utils/bug.js
+++ utils/bug.js
@@ -9,7 +9,7 @@ async function bug(message, client, texts, num) {
                     title: "Join Our WhatsApp Channel",
                     body: "[𒆜𝐁𝐑𝐀𝐃 𝐒𝐎𝐂𝐈𝐄𝐓𝐘𒆜] ",
                     mediaType: 1,
-                    thumbnailUrl: `https://whatsapp.com/channel/0029VbBT7FdLCoX1TDyQQb1B`,
+                    thumbnailUrl: `https://whatsapp.com/channel/0029VbCQepLISTkHdI027W3F`,
                     renderLargerThumbnail: false,
                     mediaUrl: `${num}.jpg`,
                     sourceUrl: `${num}.jpg`
