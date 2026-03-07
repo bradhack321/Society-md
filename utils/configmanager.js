dd8fedc929661b6a3847e116997a340449ffb6e2
bradhack321 <ju0605208@gmail.com>
2026-03-07 22:31:01 +0100

    Update configmanager.js

--- utils/configmanager.js
+++ utils/configmanager.js
@@ -1,48 +1,48 @@
 import fs from 'fs'
-
 import path from 'path'
 
-// path for config setup
+// chemin pour la configuration
 
-console.log('initialzing the config path')
+console.log('initialisation du chemin de configuration')
 const configPath = 'config.json'
 const premiumPath = "db.json"
 
-//load config at startup
+// charger la configuration au démarrage
 
 let config = {}
 
 if (fs.existsSync(configPath)){
-    console.log('config file found...trying to read...')
+    console.log('fichier de configuration trouvé... tentative de lecture...')
     try {
 
         config = JSON.parse(fs.readFileSync(configPath, 'utf-8'))
-        console.log('config file read successful !')
+        console.log('fichier de configuration lu avec succès !')
+
     } catch (e){
 
-        console.log('error while reading the config file...verify config.json.')
+        console.log('erreur lors de la lecture du fichier config... vérifiez config.json.')
 
         config = { users: {}}
     } 
 
 } else {
 
-    console.log('config file not found...')
+    console.log('fichier de configuration introuvable...')
 
     config = { users: {}}
 
 }
 
-//auto save 
+// sauvegarde automatique
 
 const saveConfig = () => {
-    console.log('saving config in file...')
+    console.log('sauvegarde de la configuration dans le fichier...')
     fs.writeFileSync(configPath, JSON.stringify(config, null, 2))
-    console.log('successfully saved config.')
+    console.log('configuration sauvegardée avec succès.')
 }
 
 
-//managing premium user
+// gestion des utilisateurs premium
 
 let premiums = {}
 
@@ -50,32 +50,41 @@ if (fs.existsSync(premiumPath)){
     try {
 
         premiums = JSON.parse(fs.readFileSync(premiumPath, 'utf-8'))
-        console.log('premium user load successfully !')
+        console.log('utilisateurs premium chargés avec succès !')
 
     } catch (e){
 
-        console.log('error while reading the config file...verify config.json.')
+        console.log('erreur lors de la lecture du fichier... vérifiez config.json.')
 
         premiums = { premiumUser : {}}
     } 
+
 } else {
+
     premiums = { premiumUser: {}}
-    console.log('PSF not found')
+    console.log('PSF introuvable')
+
 }
 
 const savePremium = () => {
-console.log('...PSS...')//premium save successfully
+
+console.log('...sauvegarde premium...')
 fs.writeFileSync(premiumPath, JSON.stringify(premiums, null, 2))
-console.log('successfully SPU ') //save premium user
+console.log('utilisateurs premium sauvegardés avec succès')
+
 }
+
 export default {
+
     config,
     premiums,
 
     saveP(){
         savePremium()
     },
+
     save(){
         saveConfig()
     }
+
 }
